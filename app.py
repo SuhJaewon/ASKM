af6c3034811e7c62d7f139fe5cdac0faf59b2b68
SuhJaewon <97786871+SuhJaewon@users.noreply.github.com>
2023-05-28 23:59:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,53 +1,64 @@
 import streamlit as st
-import pytesseract
-import re
-import joblib
-import sqlite3
 import cv2
+import pytesseract
 import numpy as np
 from PIL import Image
+from skimage import filters
 
+Image.MAX_IMAGE_PIXELS = None
+
+# Tesseract OCR 엔진 경로 설정
+pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
+
+# PIL 이미지를 OpenCV의 numpy 배열로 변환하는 함수
+def conv_pil_to_cv(img: Image) -> np.ndarray:
+    return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+
+# 이미지 전처리 함수
+def preprocess_image(image: Image) -> Image:
+    # 이미지 업스케일링
+    upscale_ratio = 4
+    resized_image = image.resize((image.width * upscale_ratio, image.height * upscale_ratio))
+
+    # 이미지 선명도 향상
+    sharpened_image = cv2.GaussianBlur(conv_pil_to_cv(resized_image), (0, 0), 3)
+    sharpened_image = cv2.addWeighted(conv_pil_to_cv(resized_image), 1.5, sharpened_image, -0.5, 0)
+
+    # OpenCV의 numpy 배열을 PIL 이미지로 변환하여 반환
+    return Image.fromarray(cv2.cvtColor(sharpened_image, cv2.COLOR_BGR2RGB))
+
+# 후처리 작업 함수
+def postprocess_text(text: str) -> str:
+    # 특정 패턴 인식 또는 문법적 규칙 적용 등의 후처리 작업을 수행할 수 있습니다.
+    return text
 
 # 기능 1: 영수증 자동 분석 기능
 def receipt_analysis():
-    # 사용자 인터페이스 구현
-    st.title('영수증 분석기')
+    st.title("영수증 자동 분석")
+    st.write("영수증 이미지를 업로드하여 자동 분석합니다.")
 
+    uploaded_image = st.file_uploader("영수증 이미지 업로드", type=["jpg", "jpeg", "png"])
 
-    # 사진 촬영 및 처리 코드 작성
-    st.write("사진을 촬영해주세요.")
+    if uploaded_image is not None:
+        image = Image.open(uploaded_image)
 
-    # 사진 촬영 버튼 클릭 시
-    if st.button('사진 촬영'):
-        # 카메라 초기화 및 사진 촬영
-        camera = cv2.VideoCapture(0)
-        _, frame = camera.read()
+        # 이미지 전처리
+        preprocessed_image = preprocess_image(image)
 
-        # 이미지 저장
-        cv2.imwrite('captured_image.jpg', frame)
+        # 전처리된 이미지 출력
+        st.image(preprocessed_image, caption="전처리된 이미지", use_column_width=True)
 
-        # 이미지 크기 조정
-        image = Image.open('captured_image.jpg')
-        resized_image = image.resize((800, 600))
-        resized_image.save('resized_image.jpg')
+        my_config="-l eng+vie --oem 3 --psm 6"
 
-        # 이미지 이진화
-        gray_image = cv2.cvtColor(np.array(resized_image), cv2.COLOR_BGR2GRAY)
-        threshold_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-        cv2.imwrite('threshold_image.jpg', threshold_image)
+        # OCR 적용
+        extracted_text = pytesseract.image_to_string(preprocessed_image,config=my_config)  # 영어와 베트남어 언어 모델 설정
 
-        # OCR을 이용한 텍스트 추출
-        text = pytesseract.image_to_string(threshold_image, lang='kor')
+        # 후처리 작업
+        processed_text = postprocess_text(extracted_text)
 
-        # 처리된 결과 표시
-        st.write("OCR 처리된 텍스트 결과:")
-        st.write(text)
-
-    
-
-    
-
-
+        # 추출된 텍스트 출력
+        st.subheader("추출된 텍스트")
+        st.write(processed_text)
 
 
 # 기능 2: 수입과 지출 관리
