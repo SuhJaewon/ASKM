4e2eafb6cec2d2d1829ab6e7d13316934cac8ff3
SuhJaewon <97786871+SuhJaewon@users.noreply.github.com>
2023-06-10 02:49:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 import re
 import csv
 import matplotlib.pyplot as plt
-
+col1,col2 = st.columns([4,5])
 # Streamlit 애플리케이션에 한글 폰트 적용
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
@@ -28,47 +28,47 @@ def preprocess_image(image: Image) -> Image:
 
     return Image.fromarray(cv2.cvtColor(enhanced_image, cv2.COLOR_BGR2RGB))
 
-def receipt_analysis():
-    st.title("영수증 분류기")
-    st.write("영수증 이미지를 업로드하여 수량과 가격을 분류합니다.")
+def receipt_analysis(total_budget):
+    with col2 :
+        st.info("영수증 이미지를 업로드하여 수량과 가격을 분류합니다.")
+        uploaded_image = st.file_uploader("영수증 이미지 업로드", type=["jpg", "jpeg", "png"])
 
-    uploaded_image = st.file_uploader("영수증 이미지 업로드", type=["jpg", "jpeg", "png"])
+        if uploaded_image is not None:
+            image = Image.open(uploaded_image)
 
-    if uploaded_image is not None:
-        image = Image.open(uploaded_image)
+            preprocessed_image = preprocess_image(image)
 
-        preprocessed_image = preprocess_image(image)
+            image_cv = conv_pil_to_cv(preprocessed_image)
 
-        image_cv = conv_pil_to_cv(preprocessed_image)
+            image_gray = cv2.cvtColor(image_cv, cv2.COLOR_BGR2GRAY)
+            blur = cv2.GaussianBlur(image_gray, ksize=(11, 11), sigmaX=0)
+            ret, thresh1 = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
+            edged = cv2.Canny(blur, 10, 250)
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+            closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+            contours, _ = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        image_gray = cv2.cvtColor(image_cv, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(image_gray, ksize=(11, 11), sigmaX=0)
-        ret, thresh1 = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
-        edged = cv2.Canny(blur, 10, 250)
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
-        closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
-        contours, _ = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            x_min, x_max = sys.maxsize, -sys.maxsize
+            y_min, y_max = sys.maxsize, -sys.maxsize
 
-        x_min, x_max = sys.maxsize, -sys.maxsize
-        y_min, y_max = sys.maxsize, -sys.maxsize
+            for contour in contours:
+                x, y, w, h = cv2.boundingRect(contour)
+                x_min = min(x_min, x)
+                x_max = max(x_max, x + w)
+                y_min = min(y_min, y)
+                y_max = max(y_max, y + h)
 
-        for contour in contours:
-            x, y, w, h = cv2.boundingRect(contour)
-            x_min = min(x_min, x)
-            x_max = max(x_max, x + w)
-            y_min = min(y_min, y)
-            y_max = max(y_max, y + h)
+            trimmed_image = image_cv[y_min:y_max, x_min:x_max]
+            trimmed_image_pil = Image.fromarray(cv2.cvtColor(trimmed_image, cv2.COLOR_BGR2RGB))
 
-        trimmed_image = image_cv[y_min:y_max, x_min:x_max]
-        trimmed_image_pil = Image.fromarray(cv2.cvtColor(trimmed_image, cv2.COLOR_BGR2RGB))
+            st.image(trimmed_image_pil, caption="자른 이미지", use_column_width=True)
 
-        st.image(trimmed_image_pil, caption="자른 이미지", use_column_width=True)
+            my_config = "-l new+new1 --oem 1 --psm 4 -c preserve_interword_spaces=1"
 
-        my_config = "-l new+new1 --oem 1 --psm 4 -c preserve_interword_spaces=1"
+            extracted_text = pytesseract.image_to_string(trimmed_image_pil, config=my_config)
 
-        extracted_text = pytesseract.image_to_string(trimmed_image_pil, config=my_config)
-
-        return extracted_text
+            product_info_list = income_expense_management(extracted_text, total_budget)
+            budget_management(product_info_list)
 
 def extract_product_info(text):
     item_pattern = r'([\w\s]+)\s+(\d+\.\d+)\s+([\d,]+)'
@@ -83,7 +83,7 @@ def extract_product_info(text):
 
     return product_info
 
-def income_expense_management(ocr_text):
+def income_expense_management(ocr_text, total_budget):
     productinfos = extract_product_info(ocr_text)
 
     with open('dataset.csv', encoding='utf8') as f:
@@ -97,14 +97,13 @@ def income_expense_management(ocr_text):
             st.write(f'수량: {quantity}')
             st.write(f'가격: {price} 동')
             st.write('---')
-    totalapp = st.text_input('예산을 입력해주세요', value='0')
     totalspn = 0
     for row in productinfos:
         totalspn += int(row[2])
-        if totalspn > int(totalapp):
+        if totalspn > int(total_budget):
             st.write('예산 초과!')
             break
-    moneyleft = int(totalapp) - int(totalspn)
+    moneyleft = int(total_budget) - int(totalspn)
     foodspend = 0
     otherspend = 0
     for row in productinfos:
@@ -135,29 +134,55 @@ def income_expense_management(ocr_text):
     return productinfos
 
 def budget_management(product_info_list):
-    # 예산 관리 기능을 구현해야 합니다.
-    pass
+    total_income = 0
+    total_expense = 0
+    category_expenses = {}
+
+    for product_info in product_info_list:
+        _, quantity, price = product_info
+        amount = quantity * price
+
+        if amount > 0:
+            total_income += amount
+        else:
+            total_expense += abs(amount)
+
+            category = "기타"  # 기본 카테고리 설정
+            if "식품" in product_info[0]:
+                category = "식품"
+
+            if category in category_expenses:
+                category_expenses[category] += abs(amount)
+            else:
+                category_expenses[category] = abs(amount)
+
+    remaining_budget = total_income - total_expense
+
+    st.write("총 수입: ", total_income)
+    st.write("총 지출: ", total_expense)
+    st.write("예산 잔액: ", remaining_budget)
+
+    if remaining_budget < 0:
+        st.write("예산 초과!")
+    else:
+        st.write("예산 여유 있음!")
+
+    # 카테고리별 지출 시각화
+    labels = list(category_expenses.keys())
+    values = list(category_expenses.values())
+
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    ax.set_xlabel("카테고리")
+    ax.set_ylabel("지출")
+    ax.set_title("카테고리별 지출")
+    st.pyplot(fig)
 
 def main():
-    st.title("ASKM")
-
-    menu = st.sidebar.selectbox("메뉴", ["영수증 자동 분석", "수입과 지출 관리", "예산 관리"])
-
-    if menu == "영수증 자동 분석":
-        ocr_text = receipt_analysis()
-        if ocr_text:
-            product_info_list = income_expense_management(ocr_text)
-            budget_management(product_info_list)
-
-    elif menu == "수입과 지출 관리":
-        st.write("수입과 지출 관리 기능을 선택하셨습니다.")
-        ocr_text = st.text_input("OCR 추출 텍스트 입력")
-        if ocr_text:
-            product_info_list = income_expense_management(ocr_text)
-            budget_management(product_info_list)
-
-    elif menu == "예산 관리":
-        budget_management([])
-
+    with col1 :
+        st.title("ASKM")
+        total_budget = st.text_input('예산을 입력해주세요', value='0')
+        if total_budget:
+            receipt_analysis(total_budget)
 if __name__ == "__main__":
     main()
